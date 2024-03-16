b66f9ca3794c4a8eefce33e393899dce85e05768
Ondřej Mekina <mekina@duck.com>
2024-03-16 18:48:02 +0100

    Update linux_system.sh

--- linux_system.sh
+++ linux_system.sh
@@ -3,3 +3,4 @@ who | sed -n "s/^\([a-z.]\+\) \+\([a-z/0-9]\+\) \+\([0-9-]\+ [0-9:]\+\) (\([0-9\
 
 # V4 dig with just the address(es) output and copy to clipboard on Wayland
 dig mekina.cz | grep -o "\([0-9]\{1,3\}\\.\)\{3,3\}[0-9]\{1,3\}$" | wl-copy
+# or just dig +short
